e271949f3476dc3abb4964d440496b59146083bb
Abdelmalek Marwan <AbdelmalekM@techprocess.net>
2024-01-15 02:20:21 +0200

    INIT

---
+++ GrpcService/Services/EmployeeSErvice.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+
+namespace GrpcService.Services
+{
+    public class EmployeeSErvice : Employee.EmployeeBase
+    {
+        public EmployeeSErvice()
+        {
+            
+        }
+
+        public override Task<EmployeeDto> GetEmployeeInfo(EmployeeinputDto request, ServerCallContext context)
+        {
+            return base.GetEmployeeInfo(request, context);
+        }
+    }
+}
